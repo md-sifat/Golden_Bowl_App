7e63fe7bb915ca8b8d578e77ac08bee35ede27f6
MD MOSTAFIZUR RAHMAN ZAHID <88180630+mostafizur-zahid@users.noreply.github.com>
2025-04-11 23:46:43 +0600

    Update orders.dart
    Create basic OrdersPage structure with state setup

--- lib/orders.dart
+++ lib/orders.dart
@@ -1,1 +1,26 @@
-import 
+import 'package:flutter/material.dart';
+
+class OrdersPage extends StatefulWidget {
+  const OrdersPage({super.key});
+
+  @override
+  State<OrdersPage> createState() => _OrdersPageState();
+}
+
+class _OrdersPageState extends State<OrdersPage> {
+  List<dynamic> orders = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Orders'), centerTitle: true),
+      body: const Center(child: Text('Loading Orders...')),
+    );
+  }
+}
